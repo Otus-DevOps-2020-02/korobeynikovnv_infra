179746c280bc78affabc9177ada94794ad23fb27
korobeynikov.nv@gmail.com <korobeynikov.nv@gmail.com>
2020-10-01 06:37:49 -0700

    moved check for updates into separate script

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#wait for auto updates to finish
-apt-get check >/dev/null 2>&1
-while [ "$?" -ne 0 ]
-do
-  echo ++++++awaiting auto updates
-  sleep 2 
-  apt-get check >/dev/null 2>&1
-done
-
-apt-get update
-apt-get install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sleep 30
